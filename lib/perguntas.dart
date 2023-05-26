dbb97cb557365ca68c12feae139e3af4e00ebf9b
Giuliana Leon <giuliana_leon1@hotmail.com>
2023-05-25 22:00:05 -0300

    att

---
+++ lib/perguntas.dart
@@ -0,0 +1,12 @@
+class Perguntas {
+  late String questao;
+  late bool resposta;
+  late String som;
+
+  Perguntas(String q, bool r, String s){
+    questao = q;
+    resposta = r;
+    som = s;
+  }
+}
+
